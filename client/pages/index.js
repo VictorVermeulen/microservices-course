f5b230d638a4da0fa380b0190cde006436c67ea0
Victor Vermeulen <vcvermeulen@gmail.com>
2022-10-15 00:08:19 +0200

    add useRequest hook

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,16 @@
-export default () => {
+const LandingPage = ({ color }) => {
+	console.log('😡', 'I am in the component', color);
 	return <h1>Landing Page</h1>;
 };
+
+// This is to execute some code on the server when url gets called, it's rendered once and that's sent to user.
+// After user sees the component, component can do it's work.
+// This is executed during server side rendering process, client won't see this
+LandingPage.getInitialProps = () => {
+	console.log('👹', 'I am on the server!');
+
+	// This can be used as prop inside component now
+	return { color: 'red' };
+};
+
+export default LandingPage;
