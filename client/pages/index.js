6c84551eca9c1415916fe0c8b77d4bf7e566afab
Victor Vermeulen <vcvermeulen@gmail.com>
2022-10-15 22:09:42 +0200

    add axios buildclient and signin page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,26 @@
-const LandingPage = ({ color }) => {
-	console.log('😡', 'I am in the component', color);
-	return <h1>Landing Page</h1>;
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+	console.log('😡', currentUser);
+	// axios.get('/api/users/currentuser').catch((error) => console.log(error));
+
+	return currentUser ? (
+		<h1>You are signed in</h1>
+	) : (
+		<h1>You are not signed in</h1>
+	);
 };
 
 // This is to execute some code on the server when url gets called, it's rendered once and that's sent to user.
 // After user sees the component, component can do it's work.
 // This is executed during server side rendering process, client won't see this
-LandingPage.getInitialProps = () => {
-	console.log('👹', 'I am on the server!');
+LandingPage.getInitialProps = async (context) => {
+	const client = buildClient(context);
+	const { data } = await client
+		.get('/api/users/currentuser')
+		.catch((error) => console.log(error));
 
-	// This can be used as prop inside component now
-	return { color: 'red' };
+	return data;
 };
 
 export default LandingPage;
