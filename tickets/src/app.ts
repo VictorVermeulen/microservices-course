6545aafe7900559b7ed8dc69444168b64e6c6c07
Victor Vermeulen <vcvermeulen@gmail.com>
2022-10-19 21:41:36 +0200

    add tickets service

---
+++ tickets/src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError } from '@vvtickets/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+
+// we store the session data within a cookie (from req.session so jwt token in cookie)
+app.use(
+	cookieSession({
+		signed: false,
+		secure: process.env.NODE_ENV !== 'test',
+	})
+);
+
+app.all('*', async (req, res) => {
+	throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
