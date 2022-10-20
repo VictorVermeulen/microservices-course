42b02694b176281a934fb4a9925534e6fccd8023
Victor Vermeulen <vcvermeulen@gmail.com>
2022-10-20 19:57:40 +0200

    add some tests for newticket route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@vvtickets/common';
+import { errorHandler, NotFoundError, currentUser } from '@vvtickets/common';
+import { createTicketRouter } from './routes/__test__/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -15,6 +16,9 @@ app.use(
 		secure: process.env.NODE_ENV !== 'test',
 	})
 );
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all('*', async (req, res) => {
 	throw new NotFoundError();
