55ece43e125b2b3d83f2383a5acbffa60d286c57
Victor Vermeulen <vcvermeulen@gmail.com>
2022-11-09 21:19:13 +0100

    add event publisher tickets

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -7,6 +7,8 @@ import {
 	validateRequest,
 } from '@vvtickets/common';
 
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../nats-wrapper';
 import { Ticket } from '../models/ticket';
 
 const router = express.Router();
@@ -39,7 +41,12 @@ router.put(
 		});
 
 		await ticket.save();
-
+		new TicketUpdatedPublisher(natsWrapper.client).publish({
+			id: ticket.id,
+			title: ticket.title,
+			price: ticket.price,
+			userId: ticket.userId,
+		});
 		res.send(ticket);
 	}
 );
