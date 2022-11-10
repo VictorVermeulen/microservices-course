105929fee6ca3c6d7a689d28076d7abf11c4c937
Victor Vermeulen <vcvermeulen@gmail.com>
2022-11-10 21:03:23 +0100

    add event publish unit test

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import mongoose from 'mongoose';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('returns 404 if provided id does not exist', async () => {
 	const id = new mongoose.Types.ObjectId().toHexString();
@@ -69,4 +70,21 @@ it('updates the ticked provided valid inputs', async () => {
 
 	expect(ticketResponse.body.title).toEqual('titletest');
 	expect(ticketResponse.body.price).toEqual(10);
+});
+
+it('publishes an event', async () => {
+	const cookie = global.signup();
+
+	const response = await request(app)
+		.post(`/api/tickets`)
+		.set('Cookie', cookie)
+		.send({ title: 'jlasfd', price: 20 });
+
+	await request(app)
+		.put(`/api/tickets/${response.body.id}`)
+		.set('Cookie', cookie)
+		.send({ title: 'titletest', price: 10 })
+		.expect(200);
+
+	expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
