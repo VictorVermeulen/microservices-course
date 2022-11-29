3db1d3e482667aee392dd456c167d7ef488a5ab3
Victor Vermeulen <vcvermeulen@gmail.com>
2022-11-29 21:48:52 +0100

    add unit test listener

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -8,14 +8,13 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
 	queueGroupName = queueGroupName;
 
 	async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
-		const { title, price, id } = data;
-
-		const ticket = await Ticket.findById(id);
+		const ticket = await Ticket.findByEvent(data);
 
 		if (!ticket) {
 			throw new Error('Ticket not found');
 		}
 
+		const { title, price } = data;
 		ticket.set({ title, price });
 		await ticket.save();
 
--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 import { Order, OrderStatus } from './order';
 
 // this is VERY specific for order service so we canNOT have this in shared common library
@@ -12,11 +13,13 @@ interface TicketAttrs {
 export interface TicketDoc extends mongoose.Document {
 	title: string;
 	price: number;
+	version: number;
 	isReserved(): Promise<boolean>;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
 	build(attrs: TicketAttrs): TicketDoc;
+	findByEvent(event: { id: string; version: number }): Promise<TicketDoc | null>;
 }
 
 const ticketSchema = new mongoose.Schema(
@@ -41,6 +44,15 @@ const ticketSchema = new mongoose.Schema(
 	}
 );
 
+ticketSchema.set('versionKey', 'version');
+ticketSchema.plugin(updateIfCurrentPlugin);
+
+ticketSchema.statics.findByEvent = (event: { id: string; version: number }) => {
+	return Ticket.findOne({
+		_id: event.id,
+		version: event.version - 1,
+	});
+};
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
 	return new Ticket({ _id: attrs.id, title: attrs.title, price: attrs.price });
 };
