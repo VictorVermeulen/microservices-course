340c85cfa89c8df3bf2586ed0d904c5f8ef84010
Victor Vermeulen <vcvermeulen@gmail.com>
2022-12-07 08:07:43 +0100

    start payments service

---
+++ payments/src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@vvtickets/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+
+// we store the session data within a cookie (from req.session so jwt token in cookie)
+app.use(
+	cookieSession({
+		signed: false,
+		secure: process.env.NODE_ENV !== 'test',
+	})
+);
+app.use(currentUser);
+
+app.all('*', async (req, res) => {
+	throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
