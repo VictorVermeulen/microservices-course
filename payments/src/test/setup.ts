bd12a0b2904f7ee502507d223b6ac7d420a4fce6
Victor Vermeulen <victor@minox.nl>
2022-12-12 08:11:34 +0100

    add stripe sdk

--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -1,53 +1,53 @@
-import { MongoMemoryServer } from 'mongodb-memory-server';
-import mongoose from 'mongoose';
-import request from 'supertest';
-import { app } from '../app';
-import jwt from 'jsonwebtoken';
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../app";
+import jwt from "jsonwebtoken";
 
 declare global {
-	var signup: () => string[];
+  var signup: (id?: string) => string[];
 }
 
-jest.mock('../nats-wrapper');
+jest.mock("../nats-wrapper");
 
 let mongo: any;
 beforeAll(async () => {
-	process.env.JWT_KEY = 'asflkjasdf';
+  process.env.JWT_KEY = "asflkjasdf";
 
-	mongo = await MongoMemoryServer.create();
-	const mongoUri = await mongo.getUri();
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = await mongo.getUri();
 
-	await mongoose.connect(mongoUri, {});
+  await mongoose.connect(mongoUri, {});
 });
 
 beforeEach(async () => {
-	jest.clearAllMocks();
-	const collections = await mongoose.connection.db.collections();
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
 
-	for (let collection of collections) {
-		await collection.deleteMany({});
-	}
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
 });
 
 afterAll(async () => {
-	if (mongo) {
-		await mongo.stop();
-	}
-	await mongoose.connection.close();
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
 });
 
-global.signup = () => {
-	const payload = {
-		id: new mongoose.Types.ObjectId().toHexString(),
-		email: 'test@test.com',
-	};
-	const token = jwt.sign(payload, process.env.JWT_KEY!);
+global.signup = (id?: string) => {
+  const payload = {
+    id: id || new mongoose.Types.ObjectId().toHexString(),
+    email: "test@test.com",
+  };
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-	const session = { jwt: token };
+  const session = { jwt: token };
 
-	const sessionJSON = JSON.stringify(session);
+  const sessionJSON = JSON.stringify(session);
 
-	const base64 = Buffer.from(sessionJSON).toString('base64');
+  const base64 = Buffer.from(sessionJSON).toString("base64");
 
-	return [`session=${base64}`];
+  return [`session=${base64}`];
 };
