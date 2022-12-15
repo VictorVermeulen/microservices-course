dedffb7b2f972a28c16aec497df3a05fc25d084d
Victor Vermeulen <vcvermeulen@gmail.com>
2022-12-15 22:04:22 +0100

    add stripe secret and charge on payment

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -8,6 +8,7 @@ import {
 	NotFoundError,
 	OrderStatus,
 } from '@vvtickets/common';
+import { stripe } from '../stripe';
 import { Order } from '../models/order';
 
 const router = express.Router();
@@ -33,6 +34,12 @@ router.post(
 		if (order.status === OrderStatus.Cancelled) {
 			throw new BadRequestError('Cannot pay for an cancelled order');
 		}
+
+		await stripe.charges.create({
+			amount: order.price * 100,
+			currency: 'usd',
+			source: token,
+		});
 
 		res.status(201).send({ success: true });
 	}
