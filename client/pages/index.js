7f51806a6b4a957ac4544e2c96ae987576051806
Victor Vermeulen <vcvermeulen@gmail.com>
2022-12-19 07:58:27 +0100

    fix some initial props code and start new ticket page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,3 @@
-import buildClient from '../api/build-client';
-
 const LandingPage = ({ currentUser }) => {
 	console.log('😡', currentUser);
 	// axios.get('/api/users/currentuser').catch((error) => console.log(error));
@@ -14,13 +12,8 @@ const LandingPage = ({ currentUser }) => {
 // This is to execute some code on the server when url gets called, it's rendered once and that's sent to user.
 // After user sees the component, component can do it's work.
 // This is executed during server side rendering process, client won't see this
-LandingPage.getInitialProps = async (context) => {
-	const client = buildClient(context);
-	const { data } = await client
-		.get('/api/users/currentuser')
-		.catch((error) => console.log(error));
-
-	return data;
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+	return {};
 };
 
 export default LandingPage;
---
+++ client/pages/tickets/new.js
@@ -0,0 +1,9 @@
+const NewTicket = () => {
+	return (
+		<div>
+			<h1>Hello New Ticket</h1>
+		</div>
+	);
+};
+
+export default NewTicket;
