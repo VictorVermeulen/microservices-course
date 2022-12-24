6fe50547340237c2f7fc5dba6ce58677ffa538d9
Victor Vermeulen <vcvermeulen@gmail.com>
2022-12-24 18:25:50 +0100

    finish frontend

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,8 +1,18 @@
 import { useEffect, useState } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import Router from 'next/router';
+import useRequest from '../../hooks/useRequest';
 
 const OrderShow = ({ order, currentUser }) => {
 	const [timeLeft, setTimeLeft] = useState(0);
+	const { doRequest, errors } = useRequest({
+		url: '/api/payments',
+		method: 'post',
+		body: {
+			orderId: order.id,
+		},
+		onSuccess: (payment) => Router.push('/orders'),
+	});
 
 	useEffect(() => {
 		const findTimeLeft = () => {
@@ -26,11 +36,12 @@ const OrderShow = ({ order, currentUser }) => {
 		<div>
 			Time left to pay: {timeLeft} seconds
 			<StripeCheckout
-				token={(token) => console.log(token)}
-				stripeKey={process.env.STRIPE_PUBLIC_KEY}
+				token={({ id }) => doRequest({ token: id })}
+				stripeKey={process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY}
 				amount={order.ticket.price * 100}
 				email={currentUser.email}
 			/>
+			{errors}
 		</div>
 	);
 };
